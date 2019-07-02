e9035971bdeecf039eb3505b7bdaf80c56b732a3
Isabel <45083722+isabeelgtz@users.noreply.github.com>
2019-07-02 09:25:25 -0500

    Update setup.py

--- Simple Pendulum - Python/setup.py
+++ Simple Pendulum - Python/setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['Calibration', 'ProofSignals'],
     url="https://github.com/nightcomnit/PositionControl-SimplePendulum/tree/master/Simple%20Pendulum%20-%20Python",
     license='MIT',
-    author='Alan Sandoval Leon ;Ana Isabel Gutierrez Chavez; José Carlos López Arriaga',
+    author='Alan Sandoval Leon; Ana Isabel Gutierrez Chavez; José Carlos López Arriaga',
     author_email='nightcomnit@gmail.com',
     description='Implementation of two libraries to calibrate and apply test signals to a simple pendulum, previously '
                 'programmed in LabVIEW with FPGA.'
